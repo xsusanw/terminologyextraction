12e133c4c5dddf03fef6c58d9043291f4cf9d9cd
Vaishnavi <vs2753@nyu.edu>
2024-12-04 05:38:05 -0500

    use regex to clean data, create a functions to return in str and list form

--- model/dataCleaner.py
+++ model/dataCleaner.py
@@ -1,3 +1,20 @@
 """
 The goal of this program is to use regex to clean the data
 """
+import re
+def cleantext(filepath):
+    with open(filepath,"r", encoding = "utf8") as corpus:
+        allLines = corpus.read()
+        
+        allLines= re.sub(r"(\bHEADER: \b)[A-Z, a-z,0-9,\/,\.,\-,\:]*\n\n(\bCONTENT: \b)","",allLines)
+        allLines=re.sub(r'\b([A-Z](?:\.[A-Z]?:)*)\.?', lambda m: ''.join(m.group(0).split('.')), allLines)
+        allLines=re.sub(':', ' ', allLines)
+        return allLines
+        
+def splitLines(str):
+    allLinesArray = re.split("\.|\n",str)
+    return allLinesArray
+
+
+
+arr = cleantext('../corpuses/test.txt')
