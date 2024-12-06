782fad2752cc17401681ff850fb3c8688d642b9d
Vaishnavi <vs2753@nyu.edu>
2024-12-06 10:26:48 -0500

    finished model, to finetune

--- model/dataCleaner.py
+++ model/dataCleaner.py
@@ -7,12 +7,14 @@ def cleantext(filepath):
         allLines = corpus.read()
         
         allLines= re.sub(r"(\bHEADER: \b)[A-Z, a-z,0-9,\/,\.,\-,\:]*\n\n(\bCONTENT: \b)","",allLines)
+        
         allLines=re.sub(r'\b([A-Z](?:\.[A-Z]?:)*)\.?', lambda m: ''.join(m.group(0).split('.')), allLines)
         allLines=re.sub(':', ' ', allLines)
+        allLines = re.sub(r'\b(\w+)\.(\w+)\b', r'\1. \2', allLines)
         return allLines
         
 def splitLines(str):
-    allLinesArray = re.split("\.|\n",str)
+    allLinesArray = re.split(r"\.|\n",str)
     return allLinesArray
 
 
--- model/main.py
+++ model/main.py
@@ -4,6 +4,7 @@
 3. use YAKE to extract yake keywords 
 4. for each YAKE keyword, split each of the top 1000 keywords into adjectives and nouns. Place them into a list
 5. TBD on implementation, use the word2vec graph to get the words closest in distance. Return top 20
+TO DO: remove stopwords
 """
 
 from gensim.models import Word2Vec
@@ -11,8 +12,22 @@ import pke
 import re
 import dataCleaner as dc
 import string
+import nltk
+from nltk.corpus import stopwords
 from yake import KeywordExtractor
 import numpy as np
+
+class key:
+    def __init__ (self):
+        self.np = r'(DET)?(JJR?S?)*(NNP?S?)+'
+        self.pn = r'(NNP)*'
+        self.vb = r'(VBD?G?N?P?Z?)'
+        self.jj = r'JJR?S?'
+        self.vp = r'(IN)?(RBS?)*(VBD?G?N?P?Z?)(RBD?)'
+        self.patterns = [self.np,self.pn,self.vb,self.jj,self.vp]
+    def allPOS(self):
+        return self.patterns
+
 
 def yakeKeywords(KE,str):
     strArr = str.split("\n")
@@ -24,20 +39,116 @@ def yakeKeywords(KE,str):
 
     return allKeywords
 
+def isValid (wp):
+    """
+    Get 5 -15 keywords from the test, based on the following requirements
+Up to  consecutive words unless long proper noun
+No more than one entity
+Noun phrase, proper name, verb, adj, phrasal verb, or part of clause
+No full sentences, conjunctions, adverbs, determiner, number, preposition, or pronoun
+No base form
+Only one word for each concept
+
+    """
+    tokens = nltk.word_tokenize(wp)
+    tagged_tokens = nltk.pos_tag(tokens)
+    tags = ""
+    keys = key()
+    all = keys.allPOS()
+    for item in tagged_tokens:
+        tags +=item[1]
+        for k in all:
+            pattern = re.compile(k)
+            if pattern.match(tags):
+                return item
+    return None
+
+    
+
+
+    
+
+
 
 
 def main():
     #extractor = pke.unsupervised.YAKE()
     allText = dc.cleantext('../corpuses/training.txt')
     sentences = dc.splitLines(allText)
+    sw = set(stopwords.words('english'))
+    news_stopwords = set([
+    "breaking", "news", "journal", "times", "post", "herald", "gazette", 
+    "daily", "weekly", "monthly", "article", "editorial", "press", "media", 
+    "report", "reporting", "publication", "columnist", "coverage", "network", 
+    "wire", "agency", "source", "sources", "headline", "reuters", "associated", 
+    "ap", "nytimes", "cnn", "bbc", "fox", "npr", "bloomberg", "politico", 
+    "wsj", "guardian", "telegraph", "tribune"])
+    combinedStopwords = sw.union(news_stopwords)
     sentences = [line.translate(str.maketrans('', '', string.punctuation)).split() for line in sentences]
     w2v = Word2Vec(sentences = sentences)
-    KE = KeywordExtractor()
-    kw = np.array(yakeKeywords(KE,allText),dtype = object)
-    flat = [item for sublist in kw for item in sublist]
+    KE = KeywordExtractor(stopwords=combinedStopwords)
+    yakekeys = np.array(yakeKeywords(KE,allText),dtype = object)
+    flat = [item for sublist in yakekeys for item in sublist]
     print(flat[1])
-    rank = sorted(flat,key = lambda x:x[1],reverse=True)
-    print(rank[:500])
+    rank = sorted(flat,key = lambda x:x[1])
+    kw = {}
+    w2vSims = {}
+    indexArr = []
+    
+   # print(rank[:500])
+    i=0
+    rankNum =0
+    while i<100 and rankNum<len(rank):
+        p=rank[rankNum]
+        if isValid(p[0]):
+            kw[p[0]]=p[1]
+            indexArr.append(p[0])
+            i+=1
+        rankNum+=1
+    for i in indexArr:
+        for j in indexArr:
+            if i==j:
+                continue
+            else:
+                if i in w2vSims.keys():
+                    product = 0
+                    indivWordsi = i.split()
+                    indivwordsj = j.split()
+                    for wordi in indivWordsi:
+                        for wordj in indivwordsj:
+                            if wordi == wordj: 
+                                continue
+                            elif wordi in w2v.wv.key_to_index and wordj in w2v.wv.key_to_index:
+                                product += w2v.wv.similarity(wordi,wordj)
+                            else:
+                                continue
+
+
+                    orig = w2vSims[i]
+                    w2vSims[i] = orig+product
+                else:
+                    product = 0
+                    indivWordsi = i.split()
+                    indivwordsj = j.split()
+                    for wordi in indivWordsi:
+                        for wordj in indivwordsj:
+                            if wordi == wordj:
+                                continue
+                            elif wordi in w2v.wv.key_to_index and wordj in w2v.wv.key_to_index:
+                                product += w2v.wv.similarity(wordi,wordj)
+                            else:
+                                continue
+                    w2vSims[i]=product
+    rankwtv = sorted(w2vSims.items(), key=lambda x: x[1], reverse = True)
+    print(rankwtv)
+
+    
+    
+    
+    
+
+
+
 
 
 
